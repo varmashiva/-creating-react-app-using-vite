46e64e0c5b73d47131d60f1ec61b76919a8e01a0
Shiva Varma <shivavarma@Shivas-MacBook-Air.local>
2024-02-28 17:55:23 +0530

    Done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <center>
+        <h1>HELLO KALVIUM</h1>
+      </center>
+      
+    </div>
+  )
+}
+
+export default App
